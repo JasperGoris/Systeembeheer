115c42ca9380543a5a854513fc39b26b3861aa89
root <root@jasper-goris.uclllabs.be>
2020-10-21 20:50:06 +0000

    Emergency updates for backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,3 +6,4 @@ if echo $1 | grep -qP "\..*\.sw(p|x|px)$" ; then #for vim users:)
 fi
 cp -p --parents $1 /var/backups/inotify
 mv /var/backups/inotify$1 /var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S')
+
